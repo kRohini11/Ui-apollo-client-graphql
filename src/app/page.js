4b018a5968168b97a13060413ca05f51c7cf95b8
ROHINI KHILARE <rohini.khilare14@gmail.com>
2024-07-17 07:35:50 +0530

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,73 @@
+
+"use client"
 import Image from "next/image";
 import styles from "./page.module.css";
+import { useEffect } from "react";
+import { useQuery, useLazyQuery, gql, useMutation } from '@apollo/client'
 
+const GET_USERS = gql`
+  query Query {
+    getUsers {
+      name
+      uid
+      pwd
+      rollno
+      gen
+      hobbies
+      country
+      address
+    }
+  }
+`
+const SAVE_USER = gql`
+  mutation Mutation($data: StudentsInput) {
+    regStudents(data: $data)
+  }
+`
 export default function Home() {
+
+  // const { loading, error, data } = useQuery(GET_USERS);
+  const [fetchUsers, { loading, error, data }] = useLazyQuery(GET_USERS);
+  const [saveUser, { loading: saveUserLoading, error: saveUserError, data: saveUserData }] = useMutation(SAVE_USER);
+
+  useEffect(() => {
+    if (!loading) {
+      if (data) {
+        console.log("success", data)
+      }
+      if (error) {
+        console.log("failute", error)
+      }
+    }
+  }, [loading])
+
+  const getUsers = () => {
+    fetchUsers()
+  }
+
+  const handleUsers = () => {
+    saveUser({
+      variables: {
+
+        "data": {
+          "address": "address",
+          "country": "China",
+          "gen": "F",
+          "hobbies": "C",
+          "name": "u9",
+          "pwd": "232322",
+          "rollno": "2",
+          "uid": "u123@gmail.com"
+        }
+      }
+    })
+  }
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    <div>
+      <h2>Users</h2>
+      <button onClick={getUsers}>get Users</button>
+      <button onClick={handleUsers}>save User</button>
+    </div>
   );
 }
